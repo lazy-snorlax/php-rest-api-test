e3e903dbe62c59d26353b158bef1beffaa26b356
Anthony Morelli <blacknebula236@gmail.com>
2020-02-17 19:21:45 +1030

    Fetch single post with given id

---
+++ api/post/read_single.php
@@ -0,0 +1,34 @@
+<?php
+
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate post object
+  $post = new Post($db);
+
+  // Get ID
+  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+  // Get post
+  $post->read_single();
+
+  // Create array
+  $post_arr = array(
+    'id' => $post->id,
+    'title' => $post->title,
+    'body' => html_entity_decode($post->body),
+    'author' => $post->author,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name,
+  );
+
+  // Make JSON
+  print_r(json_encode($post_arr));
