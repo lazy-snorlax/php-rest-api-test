d3e5bf055513d58b9fbf13104f51c3f52233d66c
Anthony Morelli <blacknebula236@gmail.com>
2020-02-20 22:05:03 +1030

    Added create functionality to Categories

---
+++ api/category/create.php
@@ -0,0 +1,30 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Category.php';
+
+  $database = new Database();
+  $db = $database->connect();
+
+  $category = new Category($db);
+
+  $data = json_decode(file_get_contents("php://input"));
+
+  $category->name = $data->name;
+
+  if ($category->create()) {
+    echo json_encode(
+      array('message' => 'Post Created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Post Not Created')
+    );
+  }
+
+  
